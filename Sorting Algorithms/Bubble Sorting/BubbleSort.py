483b1c38e5ef2d745d7aa5ec0ed5ee4913ddeca6
Harsh Dabas <harshdabas1314@gmail.com>
2026-01-18 02:05:46 +0530

    Refactor BubbleSort to use list type for array

--- Sorting Algorithms/Bubble Sorting/BubbleSort.py
+++ Sorting Algorithms/Bubble Sorting/BubbleSort.py
@@ -1,7 +1,7 @@
-arr:int = [3,2,4,5,8,11,8,45,22,65,32,87,54,99,12,34,21,56]
+arr:list = [3,2,4,5,8,11,8,45,22,65,32,87,54,99,12,34,21,56]
 arrLen:int = len(arr)
 
-def bubblesort(array:int) -> int:
+def bubblesort(array:list) -> None:
     i = 0
     for i in range(arrLen-i):
         for j in range(i+1,arrLen):
@@ -14,4 +14,5 @@ def bubblesort(array:int) -> int:
     print(array)
 
 bubblesort(arr)
+
 
